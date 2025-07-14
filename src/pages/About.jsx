4d974666d8bb5fecfe1708e076e209de53b06581
SalahLaouini <linaopticlunettes@gmail.com>
2025-07-14 13:29:10 +0100

    Update the img n°1 AboutPage

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -38,16 +38,15 @@ const About = () => {
        {/* Image Wahret Zmen boutique */}
 {/* Image Wahret Zmen boutique */}
 <FadeInSection>
-  <div className="w-full px-0 sm:px-4">
+  <div className="w-full px-0 sm:px-4 flex justify-center">
     <img
       src={aboutImage1}
       alt="Wahret Zmen Boutique"
       loading="eager"
-      className="rounded-2xl border-4 border-[#c69c6d] shadow-2xl w-full h-[300px] md:h-auto object-contain transition duration-700 ease-in-out hover:scale-110 hover:brightness-110 hover:shadow-[0_20px_40px_rgba(0,0,0,0.4)] max-w-full"
+      className="rounded-2xl border-4 border-[#c69c6d] shadow-2xl w-full h-[300px] md:h-auto object-contain transition duration-700 ease-in-out hover:scale-110 hover:brightness-110 hover:shadow-[0_20px_40px_rgba(0,0,0,0.4)] md:max-w-xl lg:max-w-lg"
     />
   </div>
 </FadeInSection>
-
 
 
 
