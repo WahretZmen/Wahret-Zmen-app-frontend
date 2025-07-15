c70906820bda80013a5ea5578ea39a9da4944737
SalahLaouini <linaopticlunettes@gmail.com>
2025-07-15 12:48:52 +0100

    updates

--- src/pages/products/ProductCard.jsx
+++ src/pages/products/ProductCard.jsx
@@ -173,10 +173,10 @@ const ProductCard = ({ product }) => {
         )}
   
         <div className="text-sm font-bold text-gray-900 mt-1">
-          {product?.newPrice} DT
+          {product?.newPrice} $
           {product?.oldPrice && (
             <span className="text-gray-400 text-sm line-through ml-2">
-              {Math.round(product?.oldPrice)} DT
+              {Math.round(product?.oldPrice)} $
             </span>
           )}
         </div>
